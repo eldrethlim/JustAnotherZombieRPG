0d55842b49ff9036da71bb9482c79a4642648e56
JunXian <eldrethlim@gmail.com>
2014-08-14 07:02:37 +0000

    added devise player pages

---
+++ app/models/player.rb
@@ -0,0 +1,8 @@
+class Player < ActiveRecord::Base
+  validates_uniqueness_of :username, :email
+  validates :username, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :players
   root to: 'page#home'
 
   get 'page/about'
