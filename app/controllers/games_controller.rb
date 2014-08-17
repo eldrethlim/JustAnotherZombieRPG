0e3ac77c2ac0e8f949b7b6d150a0fd29e3b4672d
JunXian <eldrethlim@gmail.com>
2014-08-17 03:16:49 +0000

    updates to controller and model, added active games table

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,12 +3,14 @@ class GamesController < ApplicationController
 
   def index
     @players = Player.all
+    @games = Game.all
   end
 
   def setup
     @player2 = Player.find(params[:id])
 
     if Game.setup(player1: current_player, player2: @player2 )
+      @game = Game.where(player1: current_player, player2: @player2).last
       redirect_to @game, notice: "Brainssssss"
     else
       flash[:error] = "Uh oh, something went wrong creating a game!"
--- app/models/game.rb
+++ app/models/game.rb
@@ -7,8 +7,8 @@ class Game < ActiveRecord::Base
   belongs_to :map
 
   def self.setup(attrs)
-    @game = Game.create(attrs)
-    map = @game.create_map
+    game = Game.create(attrs)
+    map = game.create_map
     30.times do |y| 
       30.times do |x| 
         gridfield = map.gridfields.create(y: 30-y, x: x+1)
